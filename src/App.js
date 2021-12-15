521eccf2fd01b2093f7891521e61d81cd28870ed
fora-heims <foraheims@gmail.com>
2021-12-14 16:50:50 -0800

    React Router set up for the four NavLinks. Displaying on page and correctly routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Books from './views/Books/Books';
+import Characters from './views/Characters/Characters';
+import Films from './views/Films/Films';
+import Main from './views/Main/Main';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <NavLink to="/">Main</NavLink>
+          <NavLink to="/films">Films</NavLink>
+          <NavLink to="/characters">Characters</NavLink>
+          <NavLink to="/books">Books</NavLink>
+        </header>
+        <Switch>
+          <Route path="/films">
+            <Films />
+          </Route>
+          <Route path="/characters">
+            <Characters />
+          </Route>
+          <Route path="/Books">
+            <Books />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
